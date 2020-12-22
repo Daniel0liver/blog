c89dd531bb13366784b0fc56f7f80d4a9282fbd4
Daniel Mobills <daniel@mobills.com>
2020-12-21 22:16:36 -0300

    [MW-1579]PATCH: Ajustando cores, sumário e links do blog

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -25,9 +25,11 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © {new Date().getFullYear()}, Criado com
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://www.gatsbyjs.com" target="_blank">
+          Gatsby
+        </a>
       </footer>
     </div>
   )
